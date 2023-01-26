a7b9f60824c5bb7ec98bf34bfe439075500b8bcc
quent <quentin.portier73@gmail.com>
2023-01-26 09:40:25 +0100

    Revert "image crée"
    This reverts commit 73d12a76e2d4df067d550998783dac56759c182f.

--- dev/nodeJs/tp2/pgm3.js
+++ dev/nodeJs/tp2/pgm3.js
@@ -1,29 +1,20 @@
 const fs = require('fs-extra')
 const crypt = require('crypto')
-//
-const image = fs.readFileSync("1.jpg", "base64")
-const binImage = Buffer.from(image, 'base64').toString('binary');
-fs.writeFileSync("image.jpg", binImage, "binary")
-
+console.time('pgm')
 const parentDir = './res/'
-const arr = [];
+fs.mkdirsSync("./res/")
 for (let i = 0; i <= 500; i++) {
-    arr.push(i);
+    const str = crypt.createHash('sha256').update(i.toString()).digest('hex');
+
+    fs.mkdirs(parentDir + str, (onerror) => {
+        f(parentDir + str, str)
+    })
 }
-main(arr)
+console.timeEnd('pgm')
 
 async function f(dir, str) {
-    // const promise1 = await fs.outputFile(dir + "/todo.txt", str)
-    // const promise2 = await fs.copy("./1.jpg", dir + "/img.png");
-    return Promise.all([fs.outputFile(dir + "/todo.txt", str), fs.copy("./image.jpg", dir + "/img.png")])
-}
+    const promise1 = await fs.outputFile(dir + "/todo.txt", str)
+    const promise2 = await fs.copy("./1.jpg", dir + "/img.png");
 
-async function main(arr) {
-    console.time('pgm')
-    await Promise.all(arr.map(async i => {
-        const str = crypt.createHash('sha256').update(i.toString()).digest('hex');
-        await fs.mkdirs(parentDir + str)
-        return f(parentDir + str, str)
-    }))
-    console.timeEnd('pgm')
+    Promise.all([promise1, promise2])
 }
