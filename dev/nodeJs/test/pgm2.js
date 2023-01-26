205ef154bf37183288124fa0ad2683d7f004edcc
quent <quentin.portier73@gmail.com>
2023-01-26 09:40:30 +0100

    Revert "exo 2"
    This reverts commit c75be937da60fe161a1ab151135f8e2fe531585f.

--- dev/nodeJs/test/pgm2.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-readline.question('Quel age a tu ? ', age => {
-    console.log(`Hey there ${age}!`);
-    readline.close();
-});
