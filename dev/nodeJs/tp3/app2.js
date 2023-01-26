faaf504806a196fac206335d0f83e80f97346827
quent <quentin.portier73@gmail.com>
2023-01-26 09:40:15 +0100

    Revert "tp 3"
    This reverts commit 9eab02e637ee4cabcce519110752e673b09e9318.

--- dev/nodeJs/tp3/app2.js
+++
@@ -1,7 +0,0 @@
-const pair = new Array(50).fill('').map((e, i) => i*2 )
-console.log(pair)
-const impaire = pair.map(e => e+1)
-console.log(impaire)
-
-const moy = (pair.reduce((a,b) => a+b ,0))/pair.length
-console.log(moy)
