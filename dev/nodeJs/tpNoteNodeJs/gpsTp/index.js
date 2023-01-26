4093a4fb768c4882854755c9e28b9c06c9d38132
quent <quentin.portier73@gmail.com>
2023-01-26 15:21:47 +0100

    Revert "gps tpnote"
    This reverts commit d5a7d2b849fdde2cf1fb7ed8206014429d069a6f.

--- dev/nodeJs/tpNoteNodeJs/gpsTp/index.js
+++ dev/nodeJs/tpNoteNodeJs/gpsTp/index.js
@@ -1,11 +1,5 @@
 import readline from 'readline';
 import distance from 'gps-distance';
-import Amadeus from 'amadeus';
-import dotenv from 'dotenv';
-dotenv.config({path:"./.env"})
-console.log('env', process.env.API_KEY)
-console.log('env', process.env.API_SECRET)
-
 
 const readLineAsync = () => {
     const rl = readline.createInterface({
@@ -21,42 +15,16 @@ const readLineAsync = () => {
     });
 };
 
-async function asyncGetTokenApi() {
-    let obj;
-    const strLinks = "https://test.api.amadeus.com/v1/security/oauth2/token"
-    const res = await fetch(strLinks,
-        {
-            "method": "POST",
-            "headers": {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            "body": {
-                grant_type : 'client_credentials',
-                client_id : process.env.API_KEY,
-                client_secret :  process.env.API_SECRET
-            }
+console.log('X1')
+const x1 = await readLineAsync()
 
-        }
-    )
-    obj = await res.json();
-    return obj;
-}
+console.log('Y1')
+const y1 = await readLineAsync()
 
+console.log('X2')
+const x2 = await readLineAsync()
 
-console.log(await asyncGetTokenApi())
+console.log('Y2')
+const y2 = await readLineAsync()
 
-
-//
-// console.log('X1')
-// const x1 = await readLineAsync()
-//
-// console.log('Y1')
-// const y1 = await readLineAsync()
-//
-// console.log('X2')
-// const x2 = await readLineAsync()
-//
-// console.log('Y2')
-// const y2 = await readLineAsync()
-//
-// console.log(distance(parseFloat(x1), parseFloat(y1), parseFloat(x2), parseFloat(y2)))
+console.log(distance(parseFloat(x1), parseFloat(y1), parseFloat(x2), parseFloat(y2)))
