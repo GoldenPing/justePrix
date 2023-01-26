ecc7ed422cab14ea4e2c60537420aba24e89e905
quent <quentin.portier73@gmail.com>
2023-01-26 09:40:29 +0100

    Revert "exo 2"
    This reverts commit d669a10f5aaeb37152ac3902084b3bd5d08517f1.

--- dev/nodeJs/test/pgm2.js
+++ dev/nodeJs/test/pgm2.js
@@ -1,42 +1,9 @@
-// const readline = require('readline').createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-//
-// readline.question('Quel age a tu ? ', age => {
-//     console.log(`Tu as ${age}!`);
-//     readline.close();
-// });
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-process.stdin.on("data", data => {
-    const dateUser = data.toString().split('/')
-    if (dateUser.length === 1) {
-        process.stderr.write("error! entrer pas conforme\n")
-        return;
-    }
-    const date = findAge(dateUser);
-    process.stdout.write(date + "\n____________________________________\n")
-})
-
-
-function findAge(data) {
-
-    const date = new Date();
-
-    let year = date.getFullYear() - data[0];
-    if (year < 0) {
-        return "tu est né dans le futur Marty c'est toi ?";
-    }
-    if (data[1] > date.getMonth()) {
-        return year;
-    } else if (data[1] < date.getMonth()) {
-        return year--;
-    } else {
-        console.log(date.getDate())
-        if (data[2] >= date.getDate()) {
-            return year
-        } else if (data[1] < date.getMonth()) {
-            return year--;
-        }
-    }
-}
+readline.question('Quel age a tu ? ', age => {
+    console.log(`Hey there ${age}!`);
+    readline.close();
+});
