35e7b33abe0e95487fde7cae703a361bcb1ad74a
quent <quentin.portier73@gmail.com>
2023-01-26 09:40:27 +0100

    Revert "exo 3 promise"
    This reverts commit 61e9b84553be2070a95125d4564975f045635a28.

--- dev/nodeJs/test/promise.js
+++ dev/nodeJs/test/promise.js
@@ -1,27 +1,19 @@
 const tata = new Promise((resolve, reject) => {
     console.log("start");
-    const yolo = setTimeout(() => resolve("timeout"), 3000)
-    if (1 === 1) {
-       // clearTimeout(yolo)
+    setTimeout(() => resolve("timeout"), 1000)
+    if (1 === 2) {
         reject("fin")
     }
 })
 
+tata.then(result => {
+    console.log(result)
+}).catch(error => {
+    console.log(error)
+})
 
-// tata.then(result => {
-//     console.log(result)
-// }).catch(error => {
-//
-//     console.log(error)
-// })
-
-go()
-
-async function go() {
-    try {
-        console.log(tata)
-    } catch (e) {
-        console.log(e)
-    }
-}
-
+// try{
+//     const resolve = await tata
+// }catch (e){
+//     console.log(e)
+// }
